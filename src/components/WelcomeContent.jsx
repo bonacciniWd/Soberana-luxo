5a383c06271b380d5156b996a6dedad70e98327b
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-10-18 12:50:58 -0300

    correção botões wpp

--- src/components/WelcomeContent.jsx
+++ src/components/WelcomeContent.jsx
@@ -7,7 +7,7 @@ const WelcomeContent = () => {
       <h1 className='h1'>BEM VINDA SOBERANA!</h1>
       <p className='p'>O Consumo consciente e inteligente chegou para você também.</p>
       <p className='p'> A Soberana Acessórios de Luxo oferece joias, semijoias e clutches incríveis para locação.</p>
-      <p className='p'>Temos mais de 20 anos de experiência na consultoria de beleza em acessórios e oferecemos um atendimento personalizado para madrinhas, noivas, debutantes, convidadas, formandas e para você!</p>
+      <p className='p'>Temos mais de 20 anos de experiência na consultoria de beleza em acessórios e oferecemos um atendimento personalizado para madrinhas, noivas, debutantes, convidadas e formandas!</p>
       
     </div>
   );
